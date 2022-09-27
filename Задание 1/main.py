9aac8470bb7cbf1c00ec2269d16730f66e304713
Anton <toni.aristov.00@inbox.ru>
2022-09-27 15:24:07 +0500

    1 change

--- Задание 1/main.py
+++ Задание 1/main.py
@@ -1,6 +1,6 @@
 def amortizationCounter():
-    price=int(input("Введите стоимость оборудования"))
-    years=int(input("Введите срок службы оборудования (кол-во лет)"))
+    price=int(input("Введите стоимость оборудования "))
+    years=int(input("Введите срок службы оборудования (кол-во лет) "))
     result=price/years/12
     print("Ежемесячный платеж равен", round(result, 2))
    
